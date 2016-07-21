f1097760ecd51d4f9408816d7ac912a50a7b34a6
charles5231 <charlieh@slalom.com>
2016-07-21 12:04:13 -0700

    changing greaterThan implementation details

--- src/utils/compare-utils.js
+++ src/utils/compare-utils.js
@@ -2,6 +2,6 @@ export function lessThan(el1, el2, comparatorFn) {
     return comparatorFn ? comparatorFn(el1, el2) < 0 : (el1 - el2 < 0);
 }
 
-export function greaterThan(first, second, comparatorFn) {
-    return comparatorFn ? comparatorFn(first, second) > 0 : first - second > 0;
+export function greaterThan(el1, el2, comparatorFn) {
+    return comparatorFn ? comparatorFn(el1, el2) > 0 : (el1 - el2 > 0);
 }
