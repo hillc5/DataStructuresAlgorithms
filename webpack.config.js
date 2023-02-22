09cb3bb8586099c4e45a6c56d58e70048d067ec4
hillc5 <charles.a.hill1@gmail.com>
2023-02-22 14:25:45 -0800

    Updating packages

--- webpack.config.js
+++ webpack.config.js
@@ -6,14 +6,5 @@ module.exports = {
         path: path.join(__dirname, 'build'),
         filename: 'build.js'
     },
-    devtool: 'inline-source-map',
-    module: {
-        loaders: [
-            {
-                test: path.join(__dirname, 'src'),
-                loader: 'babel-loader',
-                exclude: /node_modules/
-            }
-        ]
-    }
+    devtool: 'inline-source-map'
 };
